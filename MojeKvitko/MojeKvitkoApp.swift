378b3eb5ed78dd7f3b0e868cc6ddc1ff413c1191
richjindr <richard.jindrak@gmail.com>
2025-03-23 08:39:26 +0100

    Add MojeKvitko/MojeKvitkoApp.swift 1742715565927

--- MojeKvitko/MojeKvitkoApp.swift
+++ MojeKvitko/MojeKvitkoApp.swift
@@ -4,18 +4,10 @@ import SwiftUI
 struct MojeKvitkoApp: App {
     let persistenceController = PersistenceController.shared
     
-    init() {
-        // Request notification authorization on app launch
-        NotificationManager.shared.requestAuthorization()
-    }
-    
     var body: some Scene {
         WindowGroup {
-            ContentView(context: persistenceController.container.viewContext)
+            ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                .onReceive(NotificationCenter.default.publisher(for: UIApplication.willTerminateNotification)) { _ in
-                    persistenceController.saveContext()
-                }
         }
     }
 }
